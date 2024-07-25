655eedb1b64aaf18ecbf38b7c2da8a485cd423b6
Jeong Jihyo <jihyo0331@gmail.com>
2024-07-25 23:53:46 +0900

    Update main.dart

--- mkeeper/lib/main.dart
+++ mkeeper/lib/main.dart
@@ -2,22 +2,17 @@ import 'package:flutter/material.dart';
 import 'main_page.dart'; // mainpage.dart 파일 임포트
 import 'login_page.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: //Login == true ,
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
